db776fab2df74ddab9ffaead0223e87109caa1d9
nikoladot <nikolablagunovski993@gmail.com>
2021-11-06 20:12:40 +0000

    adding new maeven project config

---
+++ src/main/java/com/news/main/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.news.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TestController {
+	
+	
+	@RequestMapping(value = "/")
+	public String getTestPage() {
+		
+		return "test";
+	}
+
+}
