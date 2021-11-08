24c69955826c8c67b474f8a668ff92d451f99a27
danijel1989 <danijel.1989@hotmail.com>
2021-11-08 19:43:48 +0100

    adding adminController Category methods

--- src/main/java/com/news/main/controller/AdminController.java
+++ src/main/java/com/news/main/controller/AdminController.java
@@ -1,32 +1,55 @@
 package com.news.main.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.news.main.entity.Tag;
+import com.news.main.repository.CategoryRepository;
 import com.news.main.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.news.main.entity.Category;
-import com.news.main.service.CategoryService;
 
 @RestController
 @RequestMapping(value = "/admin")
 public class AdminController {
 	
-	@Autowired
-	private CategoryService categoryService;
+
 	@Autowired
 	private TagService tagService;
-
-	//Category Contoller
-	@RequestMapping(value = "/categories")
-	public List<Category> getAllCategories() {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+		//METHOD GET ALL CATEGORIES
+		public List<Category> getCategories(){
+			
+			List<Category> categories = new ArrayList<>();
+			categoryRepository.findAll()
+			.forEach(categories::add);
+			return categories;
+		}
 		
+		//METHOD GET ONE CATEGORY BY ID
+		public Category getCategoryById(int id) {
+			return categoryRepository.findById(id).get();
+		}
 		
-		return categoryService.getCategories();
-	}
+		//METHOD ADD NEW CATEGORY
+		public void addCategory(Category category) {
+			categoryRepository.save(category);
+		}
+		
+		//METHOD DELETE CATEGORY
+		public void deleteCategory(int id) {
+			categoryRepository.deleteById(id);
+		}
+		
+		//METHOD UPDATE CATEGORY
+		public void updateCategory(Category category, int id) {
+			categoryRepository.save(category);
+		}
 
 	//Tag Controller
 
