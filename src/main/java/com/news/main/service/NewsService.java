828de65a529136ddcadf14ccd4885bd0deb0d12a
nikoladot <nikolablagunovski993@gmail.com>
2021-11-09 17:24:14 +0000

    finished News service

--- src/main/java/com/news/main/service/NewsService.java
+++ src/main/java/com/news/main/service/NewsService.java
@@ -15,11 +15,34 @@ public class NewsService {
     NewsRepository newsRepository;
 
     public List<News> getAllNews(){
-
+// List of all tags
         List<News> news = new ArrayList<News>();
         newsRepository.findAll()
                 .forEach(news::add);
 
         return news;
     }
+// One tag by id
+    public News getNewsById(int id) {
+
+        return newsRepository.findById(id).get();
+    }
+    //method to add new post
+    public void addNews(News news) {
+
+        newsRepository.save(news);
+    }
+
+    //delete tag
+    public void deleteNews(int id) {
+
+        newsRepository.deleteById(id);
+    }
+
+    // method to update post
+    public void updateNews(News news, int id) {
+
+        newsRepository.save(news);
+    }
+
 }
